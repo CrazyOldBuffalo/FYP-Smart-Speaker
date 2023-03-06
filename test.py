7a0a15409379518cdd92d7e6803f65b06fdaefea
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2023-03-06 16:12:04 +0000

    quick corrections

--- test.py
+++ test.py
@@ -1,21 +1,20 @@
 from datetime import datetime
-#from calendarservices import calDAVServices
-#from texttospeechservice import textToSpeechService
-#from calendarParser import CalendarParser
+from calendarservices import calDAVServices
+from texttospeechservice import textToSpeechService
+from calendarParser import CalendarParser
 from datetime import datetime
 from datetime import timedelta
     
 
 def main():
-    #calDAVService = calDAVServices("http://localhost/dav.php", "test", "password")
-    #ttsEngine = textToSpeechService()
-    #ttsEngine.tester()
-    #calParser = CalendarParser()
+    calDAVService = calDAVServices("http://localhost/dav.php", "test", "password")
+    ttsEngine = textToSpeechService()
+    ttsEngine.tester()
+    calParser = CalendarParser()
     #calParser.parseICS()
     #createEvent(calDAVService)
     #value = calDAVService.getCalendars()
     #ttsEngine.speakCalendars(value)
-    print(value)
     #calendars = calDAVService.listCalendars()
     #print(calendars)
     #print(len(calDAVService.searchEventToday()))
